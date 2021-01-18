6b840c11fd7d9f83883751acadd44e59c78f3bbd
Aleks <alekkswithak@gmail.com>
2021-01-18 23:36:25 +0000

    static attempt

--- notesmanager/frontend/urls.py
+++ notesmanager/frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from frontend import views
 
-urlpatters = [
+urlpatterns = [
     path('', views.index)
 ]
--- notesmanager/notesmanager/settings.py
+++ notesmanager/notesmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
-    'rest_framework',
     'frontend',
 ]
 
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
