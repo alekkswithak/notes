db342f79bc1cafd6dc9a49af965183f609a213e5
Aleks <alekkswithak@gmail.com>
2021-01-19 15:45:22 +0000

    Fresh start

--- notesmanager/notes/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from notes.api import NoteViewSet
-
-router = routers.DefaultRouter()
-router.register('api/notes', NoteViewSet, 'notes')
-
-urlpatterns = router.urls
